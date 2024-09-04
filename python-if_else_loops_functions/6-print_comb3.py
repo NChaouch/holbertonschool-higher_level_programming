053646b4f6cb811b188d89fac84c716f7eac5cb4
NChaouch <nourchaouch44@gmail.com>
2024-09-04 19:44:11 +0000

    task 6

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for first_dg in range(0, 10):
+    # ensures that the second digit is always greater than the first.
+    for second_dg in range(first_dg + 1, 10):
+        pair = f"{first_dg}{second_dg}"
+
+        if first_dg == 8:
+            print(pair)
+        else:
+            print(pair, end=", ")
