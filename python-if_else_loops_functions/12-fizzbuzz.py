72d83ff09e0d6e3afef44d26d241ee50444579e2
NChaouch <nourchaouch44@gmail.com>
2024-09-04 18:06:43 +0000

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for number in range(1, 101):
+        string = ""
+        if number % 3 == 0: # check if number is divisible by 3
+            string += "Fizz"
+        if number % 5 == 0:
+            string += "Buzz"
+        if number % 3 != 0 and number % 5 != 0:
+            string = str(number)
+        print(string, end=" ")
+
+# call the function for diplay result
+fizzbuzz()
