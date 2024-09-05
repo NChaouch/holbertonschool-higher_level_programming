debdecbd445c8538d83909e38e9cd41f93142fb6
NChaouch <nourchaouch44@gmail.com>
2024-09-05 09:22:18 +0000

    do .format in task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for first_dg in range(10):
     # ensures that the second digit is always greater than the first.
     for second_dg in range(first_dg + 1, 10):
         if first_dg == 8 and second_dg == 9:
-            print(f"{first_dg}{second_dg}")
+            print("{}{}".format(first_dg, second_dg))
         else:
-            print(f"{first_dg}{second_dg}", end=", ")
+            print("{}{}".format(first_dg, second_dg), end=", ")
