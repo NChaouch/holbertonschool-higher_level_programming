3af8c5bbe645bee40b65b3fecdeb84efd42742f0
NChaouch <nourchaouch44@gmail.com>
2024-09-05 12:39:06 +0000

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,10 +5,7 @@ if __name__ == "__main__":
     from add_0 import add
 
     a = 1  # De©ine variable a
-    b = 2  # De©ine variable b
-
-    # Calculate the sum of the function import add from add_0.py
-    result = add(a, b)
+    b = 2  # De©ine variable 
 
     # display the result of the function
     print("{} + {} = {}".format(a, b, add(a, b))
