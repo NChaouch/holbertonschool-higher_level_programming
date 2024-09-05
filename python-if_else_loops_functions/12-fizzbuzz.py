28d08b5c76404c45c6c2ce5220a9a9598329cae1
NChaouch <nourchaouch44@gmail.com>
2024-09-05 08:09:02 +0000

    print in task 12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        string = ""
         # Check if number is divisible by both 3 and 5
         if number % 3 == 0 and number % 5 == 0:
-            string = "FizzBuzz"
+            print("FizzBuzz", end=" ")
         elif number % 3 == 0:  # Check if number is divisible by 3
-            string = "Fizz"
+            print("Fizz", end=" ")
         elif number % 5 == 0:  # Check if number is divisible by 5
-            string = "Buzz"
+            print("Buzz", end=" ")
         else:
-            string = str(number)  # If false, just print the number
+            print(number, end=" ")  # If false, just print the number
 
-        print(string, end=" ")
 
 # Call the function to display result
 
