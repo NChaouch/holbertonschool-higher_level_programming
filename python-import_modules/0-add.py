0a12e2d86191286b8437a4cba7edd06ccdf84a55
NChaouch <nourchaouch44@gmail.com>
2024-09-05 13:19:08 +0000

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-
-# Import the function add from the file add_0.py
-from add_0 import add
-
-# Define the variables a and b
-a = 1
-b = 2
-
-# Ensure the following code only runs when the file is executed directly
 if __name__ == "__main__":
-    # Calculate the result of the addition
+
+    from add_0 import add
+
+    a = 1
+    b = 2
+
     result = add(a, b)
 
-    # Display the result using string formatting
     print("{} + {} = {}".format(a, b, result))
