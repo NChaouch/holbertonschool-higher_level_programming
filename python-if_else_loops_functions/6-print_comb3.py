94e10fdfed2b14456ed10eafafd0c21399873ba0
NChaouch <nourchaouch44@gmail.com>
2024-09-05 08:24:49 +0000

    change range task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for first_dg in range(0, 10):
+for first_dg in range(10):
     # ensures that the second digit is always greater than the first.
     for second_dg in range(first_dg + 1, 10):
         pair = f"{first_dg}{second_dg}"
