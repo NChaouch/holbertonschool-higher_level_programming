e7dbac180a586d2244e51b33a5c5e386f80f2578
NChaouch <nourchaouch44@gmail.com>
2024-09-09 19:23:33 +0000

    task 9

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    int_max = my_list[0]
+    num = 1
+    while num < len(my_list):
+        if my_list[num] > int_max:
+            int_max = my_list[num]
+            num += 1
+            return int_max
