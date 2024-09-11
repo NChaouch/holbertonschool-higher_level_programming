7bb0ea2a000396c99febf191bdd365334d5c3252
NChaouch <nourchaouch44@gmail.com>
2024-09-11 08:56:08 +0000

    task 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return []
+    new_matrix = []
+    for row in matrix:
+        # for each row, create a new row where each number is squared.
+        new_row = [elmt ** 2 for elmt in row]
+        # This new line is added
+        new_matrix.append(new_row)
+        return new_matrix
