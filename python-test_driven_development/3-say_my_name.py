421d5a9a967ef0fd2cb536642a19d2245d72f77d
NChaouch <nourchaouch44@gmail.com>
2024-09-13 21:46:51 +0000

    task 3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 """
+"3-say_my_name" module.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
     Function prints 'My name is <first name> <last name>'.
 
     Args:
@@ -8,10 +14,8 @@
 
     Raises:
         TypeError: If `first_name` or `last_name` is not a string.
-"""
+    """
 
-
-def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
