db5570c81ccfc07a64ffdcefd7fbc154fe9489e7
NChaouch <nourchaouch44@gmail.com>
2024-09-14 17:17:46 +0000

    task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Function prints  a text with 2 new lines after
+each of these characters: ., ? and :
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text: The text to be processed(str).
+
+    Raises:
+        TypeError: If `text` is not a string.
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = ""
+    space_ignore = False
+
+    for punc in text:
+        if punc in ['.', '?', ':']:
+            result += punc + "\n\n"
+            space_ignore = True
+        elif punc == " " and space_ignore:
+            continue
+        else:
+            result += punc
+            space_ignore = False
+        print(result)
