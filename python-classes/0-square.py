943b477b1e45bc4fd357687077da5d8947738c7b
NChaouch <nourchaouch44@.com>
2024-09-16 08:30:04 +0000

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+class Square creation
+"""
+
+
+class Square:  # class is for declare new class, Square is name of class
+
+    """
+    empty class
+    """
+
+    pass  # is for a class empty
