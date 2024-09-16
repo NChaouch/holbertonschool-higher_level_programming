4164a1ed0ddf532f1a48d2ea3c562d2dbe121138
NChaouch <nourchaouch44@.com>
2024-09-16 09:20:19 +0000

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+class Square creation
+"""
+
+
+class Square:
+
+    """
+    self: ref of the instance in progress
+    size: size of the square
+    """
+
+    def __init__(self, size):
+
+        self.__size = size
