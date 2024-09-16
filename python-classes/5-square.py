a1eb487cd583e62e6b2b764f2b8a7f5d5d4fa50c
NChaouch <nourchaouch44@.com>
2024-09-16 17:35:22 +0000

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""
+class Square creation
+"""
+
+
+class Square:
+
+    """
+    initialize size at 0
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):  # access the value of the private size attribute
+        return self.__size
+
+    @size.setter
+    # modification of the value of private size attribute
+    def size(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):  # calcul of the update value (setter)
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):   # print the square with ###
+                print("#" * self.__size)
