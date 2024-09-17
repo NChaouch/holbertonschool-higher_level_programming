785fcf56f1c1a9febc5eef10e0cdfac01b84fcf5
NChaouch <nourchaouch44@gmail.com>
2024-09-17 12:31:52 +0000

    task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+class Rectangle creation
+"""
+
+
+class Rectangle:  # class is for declare new class, Rectangle is name of class
+
+    """
+    empty class
+    """
+
+    pass  # is for a class empty
