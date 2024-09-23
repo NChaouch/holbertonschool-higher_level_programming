f981c8b2e3ba977ea57e7b85a45444130b69f488
NChaouch <nourchaouch44@gmail.com>
2024-09-23 11:45:33 +0000

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+define the prototype
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    conditions
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
