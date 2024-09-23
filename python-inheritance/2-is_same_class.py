767d4aa34ed473561bf90f352bce9f5f8e918207
NChaouch <nourchaouch44@gmail.com>
2024-09-23 11:17:43 +0000

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+define the prototype
+"""
+
+
+def is_same_class(obj, a_class):
+
+    """
+    conditions
+    """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        False
