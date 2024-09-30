5a6f89af265ce7a13979d68da974878fd3f078b4
NChaouch <nourchaouch44@gmail.com>
2024-09-30 09:10:41 +0000

    task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+define prototype
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    code
+    """
+
+    return json.dumps(my_obj)  # dumps = json info obj
