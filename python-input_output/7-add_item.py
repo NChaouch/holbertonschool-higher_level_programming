c9849457c922548ced7d5edfc0e99919137d06a6
NChaouch <nourchaouch44@gmail.com>
2024-10-01 12:42:35 +0000

    task 7 comments

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,19 +5,20 @@ code
 """
 
 
-import sys
-import os
+import sys  # for access arg of line command
+import os  # for check if file exist
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+filename = "add_item.json"  # file of save list
 
 my_list = []
 
 if os.path.exists(filename):
+    # if exist the contain is load on my_list
     my_list = load_from_json_file(filename)
 
-my_list.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])  # contain all arg start 1
 
 save_to_json_file(my_list, filename)
