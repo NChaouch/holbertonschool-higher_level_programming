84cf9b59dc918798877d21d04245ef8b9a8b16f4
NChaouch <nourchaouch44@gmail.com>
2024-10-02 13:12:56 +0000

    task 1

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+define class and prototype's
+"""
+
+
+import pickle  # for serialization and deserialization obj
+import os  # functions for operating system
+
+"""
+code
+"""
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name:", (self.name))
+        print(f"Age:", (self.age))
+        print(f"Is Student:", (self.is_student))
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:  # mod binary write
+                pickle.dump(self, file)
+        except Exception as e:
+            print(f"Error occurred:", e)
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, 'rb') as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            print(f"File not found.")
+            return None
+        except Exception as e:
+            print(f"Error occurred while unpickling.", e)
+            return None
