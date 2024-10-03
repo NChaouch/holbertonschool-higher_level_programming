223d39d97a5ee9d7db740a4d7d27789348caa5c7
NChaouch <nourchaouch44@gmail.com>
2024-10-03 20:16:51 +0000

    task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+create class
+"""
+
+
+class Student:
+    """
+    code
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for attr in attrs:
+            if hasattr(self, attr):  # ckeck if attr is here
+                result[attr] = getattr(self, attr)  # get the value of attr
+            return result
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
