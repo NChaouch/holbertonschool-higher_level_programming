4729a9d36a1129f25657a23bb52818351a1023d8
NChaouch <nourchaouch44@gmail.com>
2024-10-14 10:39:58 +0200

    Update 404 response

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -43,7 +43,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Endpoint not found")
 
 
 PORT = 8000
