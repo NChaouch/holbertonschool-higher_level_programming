319c4bdea57629d52855c6d3e22bd7d5922afe80
NChaouch <nourchaouch44@gmail.com>
2024-10-14 10:17:59 +0200

    Pycodestyle fix

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
+import http.server
+import json
+
 """
 define prototypes and import
 """
-
-
-import http.server
-import json
 
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
@@ -16,23 +15,25 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     """
 
     def do_GET(self):
-        if self.path == "/":
+        if self.path == '/':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
-        elif self.path == "/data":
+        elif self.path == '/data':
             self.send_response(200)
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode('utf-8'))
-        elif self.path == "/info":
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/info':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
-        elif self.path == "/status":
+            info = {"version": "1.0",
+                    "description": "A simple API built with http.server"}
+            self.wfile.write(json.dumps(info).encode())
+        elif self.path == '/status':
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
