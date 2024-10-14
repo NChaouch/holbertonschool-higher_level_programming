9257c2cbd6757f4682ee798905a8cafc9c996616
NChaouch <nourchaouch44@gmail.com>
2024-10-14 09:49:19 +0200

    Test checker

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -20,7 +20,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == "/":
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == "/data":
@@ -28,16 +28,12 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-type", "application/json")
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
-            self.wfile.write(json.dumps(data).encode())
+            self.wfile.write(json.dumps(data).encode('utf-8'))
         elif self.path == "/info":
             self.send_response(200)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
-            info = {
-                "version": "1.0",
-                "description": "A simple API built with http.server"
-            }
-            self.wfile.write(json.dumps(info).encode())
+            self.wfile.write(b'{"version": "1.0", "description": "A simple API built with http.server"}')
         elif self.path == "/status":
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
@@ -45,7 +41,7 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(b"OK")
         else:
             self.send_response(404)
-            self.send_header("Content-type", "text/html")
+            self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"404 Not Found")
 
