51beef767afeec11169f87aa1bcc56a569382712
NChaouch <nourchaouch44@gmail.com>
2024-10-14 10:36:02 +0200

    Good outputs

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -43,7 +43,7 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
-            self.wfile.write(b"Endpoint not found")
+            self.wfile.write(b"404 Not Found")
 
 
 PORT = 8000
