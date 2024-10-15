dbca3197b47762e8155418604a47a7c0ed661e5a
NChaouch <nourchaouch44@gmail.com>
2024-10-15 17:19:18 +0200

    Test checker

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+
+@app.route("/status")
+def get_status():
+    return "OK"
+
+@app.route("/add_user", methods=["POST"])
+def addUser():
+    user_data = request.get_json()
+    username = user_data.get("username")
+
+    if username is None:
+        return jsonify({"error": "Username is required"})
+    
+    users[username] = user_data
+
+    return jsonify(message="User added", user=user_data)
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"})
+
+if __name__ == "__main__":
+    app.run()
