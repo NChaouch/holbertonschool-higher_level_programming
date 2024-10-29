f396cb00b230db65825f8553d7521c0f300a5227
NChaouch <nourchaouch44@gmail.com>
2024-10-29 21:02:20 +0100

    modification lists all the cities of California task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- lists all the cities of California that can be found in the database hbtn_0d_usa.
 -- creation script
-SELECT cities.id, cities.name FROM cities, states
-WHERE cities.state_id = state_id AND states.name = 'California'
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name 
+FROM cities, states
+WHERE cities.state_id = state_id AND states.name = 'California' ORDER BY cities.id ASC;
