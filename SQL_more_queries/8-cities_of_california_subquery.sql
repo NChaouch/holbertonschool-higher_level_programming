2f8e635838faa4c8ff94fdb71465326e5bd963be
NChaouch <nourchaouch44@gmail.com>
2024-10-29 21:17:37 +0100

    modification task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,5 @@
 -- creation script
 SELECT cities.id, cities.name 
 FROM cities, states
-WHERE cities.state_id = states_id AND states.name = 'California' ORDER BY cities.id ASC;
+WHERE cities.state_id = states_id AND states.name = 'California'
+ORDER BY cities.id ASC;
