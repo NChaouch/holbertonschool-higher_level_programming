b096df8dc03ee5eb85b0127b34901186a4f441d7
NChaouch <nourchaouch44@gmail.com>
2024-10-29 10:24:17 +0100

    creates the MySQL server user user_0d_1 task 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- creates the MySQL server user user_0d_1
+-- creation script
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+-- for all databases (*.*) and according all privileges at other users.
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+-- for modification is acting 
+FLUSH PRIVILEGES;
