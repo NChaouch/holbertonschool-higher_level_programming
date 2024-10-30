f96fd429499d422ccf6eebec97c3df216ac138d9
NChaouch <nourchaouch44@gmail.com>
2024-10-30 16:06:34 +0100

    database dump from hbtn_0d_tvshows task 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- database dump from hbtn_0d_tvshows
+-- creation script
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
