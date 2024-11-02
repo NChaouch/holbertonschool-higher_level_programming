17c91d612743c33b322129ce776cdb0fbe3d2ed8
NChaouch <nourchaouch44@gmail.com>
2024-11-02 22:10:35 +0000

    SQL Injection... task 3

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+import code
+"""
+
+import MySQLdb
+import sys
+
+"""
+code
+"""
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
+    for state in cursor.fetchall():
+        if str(state[1]) == sys.argv[4]:
+            print("{}".format(state))
+
+    cursor.close()
+    db.close()
