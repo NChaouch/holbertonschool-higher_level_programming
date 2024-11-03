a72cc243d6d5f44f57d9b647c5d45dc234b6c1a9
NChaouch <nourchaouch44@gmail.com>
2024-11-03 18:39:43 +0000

    task 6

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+import code
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, ForeignKey, String
+
+Base = declarative_base()
+
+"""
+code
+"""
+
+
+class State(Base):
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
